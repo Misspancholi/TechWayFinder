9878f613f0dc5aae837e1ffb1802cc15941e3f2c
Kavishm24 <kavishmenaria09@gmail.com>
2025-03-16 23:32:47 +0530

    everthing is working

--- temp/quiz.py
+++ temp/quiz.py
@@ -2,10 +2,12 @@ import streamlit as st
 import sqlite3
 
 def display_quiz():
-    # Create a selectbox for page navigation
+    if "submit" not in st.session_state:
+        st.session_state.submit = False
+    
     def get_connection():
         return sqlite3.connect('temp/TWF.db', check_same_thread=False)
-    print("-------------------------------------------------------------")
+    
     # Reading questions
     conn_read = get_connection()
     c_read = conn_read.cursor()
@@ -16,22 +18,39 @@ def display_quiz():
     questions = ques.fetchall()
     conn_read.close() 
 
+    # Initialize selections in session state if not already present
+    if 'quiz_selections' not in st.session_state:
+        st.session_state.quiz_selections = [None] * 34
+
     # Use a unique key for the form
-    with st.form("quiz_form_" + str(st.session_state.user_id) + "_" + str(st.session_state.get('form_counter', 0))):
+    with st.form("quiz_form_" + str(st.session_state.user_id)):
         st.title("Technical Skills Quiz")
         selections = []
-        for question in questions[:34]:
-            selections.append(st.radio(f"{question[1]}", [question[3], question[4], question[5], question[6]], index=0))
-        submit_button = st.form_submit_button(" i am done ")
-        print("submit button value:",submit_button)
-        
+            
+        for i, question in enumerate(questions[:34]):
+            # Use a unique key for each radio button
+            key = f"question_{i}_{st.session_state.user_id}"
+            selection = st.radio(
+                f"{question[1]}", 
+                [question[3], question[4], question[5], question[6]], 
+                key=key
+            )
+            selections.append(selection)
+            
+        submit_button = st.form_submit_button(
+            "Submit",
+            use_container_width=True
+        )
+        print("submit button", submit_button)
         if submit_button:
-            print("submit clicked")
+            st.session_state.submit = True
+            st.session_state.quiz_selections = selections
+            
             if None in selections:
-                st.error("please answer all questions.")
-                
+                st.error("Please answer all questions.")
             else:
                 st.success("Thank you for completing the quiz!")
+                
                 score = 0
                 skill_score = []
                 for i in range(34):
@@ -40,14 +59,12 @@ def display_quiz():
                         score = 0
                     if selections[i] == questions[i][3]:
                         score += questions[i][7]
-                        # print('question:', questions[i][7])
                     elif selections[i] == questions[i][4]:
                         score += questions[i][8]
                     elif selections[i] == questions[i][5]:  
                         score += questions[i][9]
                     elif selections[i] == questions[i][6]:
                         score += questions[i][10]
-                    
                 skill_score.append(score)
 
                 userid = st.session_state.user_id
@@ -69,7 +86,6 @@ def display_quiz():
                         "Troubleshooting_Skills" = ?, "Graphics_Designing" = ? 
                         WHERE user_id = ?''', (*skill_score, userid))
                     st.info("Your quiz results have been updated!")
-                    # st.session_state.result_stage='load'
                 else:
                     c.execute('''INSERT INTO quiz_scores(
                         user_id, "Database_Fundamentals", "Computer_Architecture", "Distributed_Computing_Systems",
@@ -80,10 +96,5 @@ def display_quiz():
                     ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
                     (userid, *skill_score))
                     st.info("Your quiz results have been saved!")
-                    # st.session_state.result_stage='load'
                 conn.commit()
-                conn.close()
-    # Increment the form counter
-    st.session_state.form_counter = st.session_state.get('form_counter', 0) + 1
 
-display_quiz()
